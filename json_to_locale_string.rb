9bf64f167bc5bf965e18c2bddced81eeba3c7714
ZhangJinzhu <wosmvp@gmail.com>
2009-09-06 10:51:12 +0800

    add js_locale rake

--- json_to_locale_string.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require 'yaml'
-yaml_file   = ARGV[0]
-result_file = ARGV[1]
-
-f = File.new(result_file,'w')
-f.write '__LocaleString = '
-f.write YAML.load_file(yaml_file).inspect.gsub(/\"=>\"/,'" : "')
-f.close
