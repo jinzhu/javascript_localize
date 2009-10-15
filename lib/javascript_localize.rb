281397189401d7fe0788b9c2d08c126d4dd1500f
ZhangJinzhu <wosmvp@gmail.com>
2009-10-15 18:41:27 +0800

    more function

--- lib/javascript_localize.rb
+++ lib/javascript_localize.rb
@@ -1,11 +1,53 @@
 module JavascriptLocalize
+
+  # _ = function(str,opt){
+  #   var localeString = __LocaleString[str];
+  #   localeString = (localeString && localeString != '') ? localeString : str;
+  #
+  #   if(opt){
+  #     fn = new Function("obj",
+  #         "var p=[],print=function(){p.push.apply(p,arguments);};" +
+  #
+  #         "with(obj){p.push(\"" +
+  #         localeString
+  #         .replace(/[\t]/g, " ")
+  #         .replace(/\"/g, '\\"')
+  #         .split("{{").join("\t")
+  #         .replace(/\t(.*?)}}/g, "\",function(){ try { return $1 }catch(err){ return '$1 undefined'} }.apply(this),\"")
+  #         + "\");};return p.join('');");
+  #
+  #     return fn.call(this,opt)
+  #   }else{
+  #     return localeString
+  #   }
+  # }
+
   def include_localized_javascript
-    "<script src='/javascripts/locales/#{I18n.locale}.js' type='text/javascript' charset='utf-8'></script>
-    <script type='text/javascript'>
-       _ = function(str){
-         var localeString = __LocaleString[str];
-         return (localeString && localeString != '') ? localeString : str;
-       }
-     </script>"
+    return %Q{
+      <script src='/javascripts/locales/#{I18n.locale}.js' type='text/javascript' charset='utf-8'></script>
+      <script type='text/javascript'>
+        _ = function(str,opt){
+          var localeString = __LocaleString[str];
+          localeString = (localeString && localeString != '') ? localeString : str;
+
+          if(opt){
+            fn = new Function("obj",
+                "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+                "with(obj){p.push(\\"" +
+                localeString
+                .replace(/[\\t]/g, " ")
+                .replace(/\\"/g, '\\\\"')
+                .split("{{").join("\\t")
+                .replace(/\\t(.*?)}}/g, "\\",function(){ try { return $1 }catch(err){ return '$1 undefined'} }.apply(this),\\"")
+                + "\\");};return p.join('');");
+
+            return fn.call(this,opt)
+          }else{
+            return localeString
+          }
+        }
+      </script>
+    }
   end
 end
